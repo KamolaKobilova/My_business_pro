b33ff61c4395f5d56d0a494ca643b794fa114a83
Kamola Kobilova <kamola_8880@mail.ru>
2024-02-10 10:12:57 +0500

    dropdown styles

--- src/Pages/MainHomePage/BookingModal/constan/menuConstants.tsx
+++ src/Pages/MainHomePage/BookingModal/constan/menuConstants.tsx
@@ -4,8 +4,15 @@ import {PlusSquareOutlined} from "@ant-design/icons";
 import { ProfileDrawer } from '../../ProfileDrawer';
 import AppointDrawer from '../AppointDrawer';
 import { CheckSquareOutlined, FieldTimeOutlined, BellOutlined, AppstoreOutlined } from '@ant-design/icons';
-import '../../Navbar.css'
-
+import '../../Navbar.css';
+import app from  '../assets/app.png';
+import addBr from '../assets/addBr.png';
+import addSp from '../assets/addS.png';
+import work from '../assets/Work.png';
+import staff from '../assets/Staff.png';
+import service from '../assets/serv.png';
+import customer from '../assets/cus.png';
+import res from '../assets/res.png'
 
 
 
@@ -21,21 +28,59 @@ export const MenuWithDrawer = () => {
     const onCloseDrawer = () => {
         setDrawerVisible(false);
     };
-
+   
     const menu = (
         <Menu style={{ width: "260px", height: "400px" }} onClick={handleMenuClick}>
-            <Menu.Item key="1">
-                <CheckSquareOutlined style={{ marginRight: "5px" }} />
-                Appointment
+            <Menu.Item key="1" >
+                <div style={{display:"flex",alignContent:"center", marginTop:"8px"}}>
+                <img src={app} alt="" style={{width:"30px", marginRight:"8px"}} />
+                <p style={{marginTop:"4px"}}>Appointment</p>
+                </div>
             </Menu.Item>
-            <Menu.Item key="2"><FieldTimeOutlined style={{ marginRight: "5px" }} />Add break & Block time</Menu.Item>
-            <Menu.Item key="3"><BellOutlined style={{ marginRight: "5px" }} />Add special working hours</Menu.Item>
+            <Menu.Item key="2" >
+                <div style={{display:"flex",alignContent:"center",marginTop:"8px"}}>
+                <img src={addBr} alt="" style={{width:"30px", marginRight:"8px"}} />
+                <p style={{marginTop:"4px"}}> Add break & Block time</p>
+                </div>
+            </Menu.Item>
+            
+            <Menu.Item key="3" >
+                <div style={{display:"flex",alignContent:"center",marginTop:"8px"}}>
+                <img src={addSp} alt="" style={{width:"30px", marginRight:"8px"}} />
+                <p style={{marginTop:"4px"}}>  Add special working hours</p>
+                </div>
+            </Menu.Item>
             <div style={{ width: "200px", marginLeft: "28px" }}> <hr /></div>
-            <Menu.Item key="5"><AppstoreOutlined />Workspaces</Menu.Item>
-            <Menu.Item key="6">Staff</Menu.Item>
-            <Menu.Item key="7">Service</Menu.Item>
-            <Menu.Item key="8">Customer</Menu.Item>
-            <Menu.Item key="4">Resources</Menu.Item>
+            <Menu.Item key="4" >
+                <div style={{display:"flex",alignContent:"center",marginTop:"8px"}}>
+                <img src={work} alt="" style={{width:"30px", marginRight:"8px"}} />
+                <p style={{marginTop:"4px"}}> Workspaces</p>
+                </div>
+            </Menu.Item>
+            <Menu.Item key="5" >
+                <div style={{display:"flex",alignContent:"center",marginTop:"8px"}}>
+                <img src={staff} alt="" style={{width:"30px", marginRight:"8px"}} />
+                <p style={{marginTop:"4px"}}>Staff</p>
+                </div>
+            </Menu.Item>
+            <Menu.Item key="6" >
+                <div style={{display:"flex",alignContent:"center",marginTop:"8px"}}>
+                <img src={service} alt="" style={{width:"30px", marginRight:"8px"}} />
+                <p style={{marginTop:"4px"}}>Service</p>
+                </div>
+            </Menu.Item>
+            <Menu.Item key="6" >
+                <div style={{display:"flex",alignContent:"center",marginTop:"8px"}}>
+                <img src={customer} alt="" style={{width:"30px", marginRight:"8px"}} />
+                <p style={{marginTop:"4px"}}>Customer</p>
+                </div>
+            </Menu.Item>
+            <Menu.Item key="6" >
+                <div style={{display:"flex",alignContent:"center",marginTop:"8px"}}>
+                <img src={res} alt="" style={{width:"30px", marginRight:"8px"}} />
+                <p style={{marginTop:"4px"}}>Customer</p>
+                </div>
+            </Menu.Item>
         </Menu>
     );
     const { Option } = Select;
