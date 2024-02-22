4afbe1a660d5da9120e12a0b1936675d87a8b62d
Dinora <acullen647@gmail.com>
2024-02-22 21:42:52 +0500

    used map to inputs

--- src/Pages/SignUp/SignUpForm.tsx
+++ src/Pages/SignUp/SignUpForm.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Form, Input, Checkbox, message } from "antd";
+import { Form, Checkbox, message } from "antd";
 import { NavbarSignUp } from "./NavbarSignUp";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -24,11 +24,37 @@ const SignUpForm: React.FC = () => {
     if (data) {
       dispatch(setToken(data?.token));
       navigate("/");
-      message.success("Sign up successfully!");
+      message.success("Sign up successful!");
     } else if (error) {
       console.error("Sign-up failed. Please try again.", error);
     }
   }, [data, error, dispatch, navigate]);
+
+  const inputFields = [
+    {
+      label: "First Name",
+      name: "firstName",
+      rules: [{ required: true, message: "Please enter your first name!" }],
+    },
+    {
+      label: "Last Name",
+      name: "lastName",
+      rules: [{ required: true, message: "Please enter your last name!" }],
+    },
+    {
+      label: "Email",
+      name: "email",
+      rules: [
+        { required: true, message: "Please enter your email!" },
+        { type: "email", message: "Please enter a valid email address!" },
+      ],
+    },
+    {
+      label: "Phone Number",
+      name: "phone",
+      rules: [{ required: true, message: "Please enter your phone number!" }],
+    },
+  ];
 
   return (
     <>
@@ -53,44 +79,21 @@ const SignUpForm: React.FC = () => {
           layout="vertical"
           onFinish={onFinish}
         >
-          <Form.Item
-            label="First Name"
-            name="firstName"
-            rules={[
-              { required: true, message: "Please enter your first name!" },
-            ]}
-          >
-            <AntInput />
-          </Form.Item>
-          <Form.Item
-            label="Last Name"
-            name="lastName"
-            rules={[
-              { required: true, message: "Please enter your last name!" },
-            ]}
-          >
-            <AntInput />
-          </Form.Item>
-
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              { required: true, message: "Please enter your email!" },
-              { type: "email", message: "Please enter a valid email address!" },
-            ]}
-          >
-            <AntInput />
-          </Form.Item>
-          <Form.Item
-            label="Phone Number"
-            name="phone"
-            rules={[
-              { required: true, message: "Please enter your phone number!" },
-            ]}
-          >
-            <AntInput />
-          </Form.Item>
+          {inputFields.map((field) => (
+            <Form.Item
+              key={field.name}
+              label={field.label}
+              name={field.name}
+              rules={[
+                {
+                  required: true,
+                  message: ` Please enter your ${field.label} `,
+                },
+              ]}
+            >
+              <AntInput />
+            </Form.Item>
+          ))}
           <Form.Item
             label="Password"
             name="password"
@@ -98,7 +101,6 @@ const SignUpForm: React.FC = () => {
           >
             <PasswordInpt />
           </Form.Item>
-
           <Form.Item>
             <p style={{ fontFamily: "sans-serif" }}>
               Your data will be stored in the US data center.
