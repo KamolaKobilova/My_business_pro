cd6fad6140b0a114abd26533e269f565b2c3f179
Dinora <acullen647@gmail.com>
2024-02-26 15:26:10 +0500

    used map() to service fields

--- src/Pages/MainHomePage/ServicesPage/ServiceComponents/ServiceInformation.tsx
+++ src/Pages/MainHomePage/ServicesPage/ServiceComponents/ServiceInformation.tsx
@@ -3,73 +3,22 @@ import {
   ScrollingBlockContent,
   ScrollingBlockWrp,
 } from "../../MyProfile/stylesForMyProfile";
+import { SERVICE_INFORMATION_FIELDS } from "./constants";
 
 export const ServiceInformation = () => {
   return (
-    <>
-      <ScrollingBlockWrp variant="little">
-        <div className="header">
-          <h3>Define This Service</h3>
-        </div>
-        <ScrollingBlock variant="little">
-          <ScrollingBlockContent>
-            <h3>Name Of The Service</h3>
-            <p></p>
+    <ScrollingBlockWrp variant="little">
+      <div className="header">
+        <h3>Define This Service</h3>
+      </div>
+      <ScrollingBlock variant="little">
+        {SERVICE_INFORMATION_FIELDS.map((field) => (
+          <ScrollingBlockContent key={field.key}>
+            <h3>{field.title}</h3>
+            <p>{[field.key]}</p>
           </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Allocated Workspace</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Contact Number</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Service Type</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Preferred Meeting Mode</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Reference Color</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Duration</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Price</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Pre-buffer</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Post-buffer</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Include Buffer Time In Calendar Invite</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Description</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Visibility</h3>
-            <p></p>
-          </ScrollingBlockContent>
-          <ScrollingBlockContent>
-            <h3>Status</h3>
-            <p></p>
-          </ScrollingBlockContent>
-        </ScrollingBlock>
-      </ScrollingBlockWrp>
-    </>
+        ))}
+      </ScrollingBlock>
+    </ScrollingBlockWrp>
   );
 };
